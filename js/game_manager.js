3b2304d460874cacca1bcbdd33c5fe1c1ab54681
Victor <melnikukr@mail.ru>
2014-03-14 19:51:14 +0200

    add chance to mulptiply tile value by 4

--- js/game_manager.js
+++ js/game_manager.js
@@ -127,7 +127,8 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+          var multiply = Math.random() < 0.986 ? 2 : 4;
+          var merged = new Tile(positions.next, tile.value * multiply);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
